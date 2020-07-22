8c6cbced34fa5e5915d1e40b65933a035df9e4aa
Nilesh Indore <indorenilesh@gmail.com>
2020-07-22 21:40:54 +0530

    update...

---
+++ Lec-1/functions.py
@@ -0,0 +1,30 @@
+name="Nilesh"
+age=37
+weight=65.2
+skill=["Linux","Python","Ansible"]
+detail={"Name" : "Nilesh", "age" : 37, "Skill" : skill }
+
+#type function to check datatype
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(skill))
+print(type(detail))
+
+#Mathamatical function with int and float
+x1 = 2
+x2 = 90
+ 
+print(x1+x2)    #addition for int/float is arithmatic addition
+print(x1-x2) 
+print(x1*x2)
+print(x1/x2)    #division, in python 3 the o/p is always a float number
+print(x1//x2)   #python 2 equivalent division, only intger part is returned
+print(x1**x2)   #exponent
+print(x1%x2)    #module/reminder
+
+x1 = x1 + 9     #self addition
+x1 += 9         #self addition, same as above expression
+
+
+
